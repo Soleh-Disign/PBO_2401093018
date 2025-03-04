59ae1ad8e699cb10e2b90d1e6e2e8eebe197f600
Muhamad Soleh <muhamadsolehbusiness@gmail.com>
2025-03-04 20:39:48 +0700

    Pertemuan Tiga

--- src/Soleh260225/SolehTugas4111.java
+++ src/Soleh260225/SolehTugas4111.java
@@ -11,15 +11,15 @@ package Soleh260225;
 public class SolehTugas4111 {
     public static void main(String[] args)
     {
-        int number= 10;
-        char letter= 'a';
-        boolean result=true;
-        String str ="hello";
+        int number = 10;
+        char letter = 'a';
+        boolean result = true;
+        String str = "hello";
 
-        System.out.println(" number ="+number);
-        System.out.println(" letter ="+letter);
-        System.out.println(" result ="+result);
-        System.out.println(" str ="+str);
+        System.out.println(" number = "+number);
+        System.out.println(" letter = "+letter);
+        System.out.println(" result = "+result);
+        System.out.println(" str = "+str);
         
     }
 
